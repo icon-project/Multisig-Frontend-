5fab1cac406f937f4e921b385655ae64d8b6c8fc
smrkarkii <076bct080.smriti@pcampus.edu.np>
2024-07-17 12:28:50 +0545

    Proposals shown in table and can be approved

--- src/pages/EVMManagerPage.tsx
+++ src/pages/EVMManagerPage.tsx
@@ -7,11 +7,13 @@ import { abi } from '../abi/SAFE_ABI';
 // import { getEthersSigner } from '../EtherJs/ethers';
 import { config } from '../config';
 import { getChainId } from '@wagmi/core';
+import proposal_data from '../../scripts/proposal_data.json';
+
+// import { Proposals } from '../components/ProposalsList/Proposals';
 
 import { EthereumContracts } from '../constants/contracts';
 
 const EVMManagerPage = () => {
-  const [proposalId, setProposalId] = useState('');
   const signer = useEthersSigner();
   const chainId = getChainId(config); // account, chainid, metamask
 
@@ -29,11 +31,8 @@ const EVMManagerPage = () => {
     '8453': EthereumContracts.BASE,
   };
 
-  const handleChange = (e) => {
-    setProposalId(e.target.value);
-  };
-
-  const handleSubmit = async () => {
+  const handleSubmit = async (hash) => {
+    console.log('');
     if (!signer) {
       console.error('Signer is undefined. Check initialization.');
       return;
@@ -45,9 +44,8 @@ const EVMManagerPage = () => {
       let contract = new ethers.Contract(contractList[chainId], abi, signer);
       // const bytes32proposal = ethers.utils.formatBytes32String(proposalId);
       console.log(contract, 'contracts');
-      const bytes32proposal = proposalId;
-      // console.log(bytes32proposal, 'changed to bytes32');
-      const tx = await contract.approveHash(bytes32proposal);
+
+      const tx = await contract.approveHash(hash);
       // const tx = await contract.approvedHashes();
 
       // Wait for the transaction to be mined
@@ -59,10 +57,6 @@ const EVMManagerPage = () => {
   };
 
   useEffect(() => {
-    console.log('Proposal ID:', proposalId);
-  }, [proposalId]);
-
-  useEffect(() => {
     console.log('Current chain ID:', chainId);
     let address = contractList[chainId];
     console.log('Contract address:', address);
@@ -72,7 +66,7 @@ const EVMManagerPage = () => {
     <div className="evm-manager-page">
       <div>
         <ConnectButton />
-        <div className="pl-32 pt-10">
+        {/* <div className="pl-32 pt-10">
           <p className="text-xl">Enter proposal hash to approve the proposal.</p>
           <div className="flex flex-row pt-7">
             <div>
@@ -85,15 +79,45 @@ const EVMManagerPage = () => {
                 onChange={handleChange}
               />
             </div>
-            <div className="pl-3">
-              <button
-                className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-                onClick={handleSubmit}
-              >
-                Approve proposal
-              </button>
-            </div>
           </div>
+        </div> */}
+      </div>
+      {/* //proposals List */}
+      <div>
+        <h1 className="pt-10">List of proposals</h1>
+
+        <div>
+          <table className="mt-2  ">
+            <thead>
+              <tr className="border border-black">
+                <th className="border border-black h-16">Title </th>
+                <th className="border border-black h-16">Proposal </th>
+
+                <th className="border border-black">Approve</th>
+              </tr>
+            </thead>
+
+            {proposal_data.map((proposal) => (
+              <tr className="border border-black">
+                <td className="border border-black h-16 pl-3">{proposal.remark}</td>
+                <td className="border border-black h-16 pl-3">{proposal.proposal}</td>
+
+                <td>
+                  <div className="pl-3 w-64 ">
+                    <button
+                      className="bg-transparent  hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-10 border border-blue-500 hover:border-transparent rounded"
+                      onClick={() => {
+                        handleSubmit(proposal.proposal);
+                      }}
+                    >
+                      Approve proposal
+                    </button>
+                  </div>{' '}
+                </td>
+              </tr>
+            ))}
+            {/* {!isComplete ? <SpinningCircles fill="black" className="w-20 h-10 inline fixed top-20 right-96 pl-3" /> : ''} */}
+          </table>
         </div>
       </div>
     </div>
