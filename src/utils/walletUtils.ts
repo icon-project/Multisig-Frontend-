d81c96acca12bc4eed287a237abf4c4882b1c4c5
saajand <saajanduwal2000@gmail.com>
2025-11-25 16:40:47 +0545

    Improvements: use getOfflineSignerAuto for improved account retrieval

--- src/utils/walletUtils.ts
+++ src/utils/walletUtils.ts
@@ -2,7 +2,7 @@ export const getKeplr = async (chainId: string) => {
   const keplr = (window as any).keplr;
   await keplr.enable(chainId);
 
-  const offlineSigner = keplr.getOfflineSigner(chainId);
+  const offlineSigner = await keplr.getOfflineSignerAuto(chainId);
   const accounts = await offlineSigner.getAccounts();
   const key = await keplr.getKey(chainId);
 
